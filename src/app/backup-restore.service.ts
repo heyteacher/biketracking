743064668b9abfa6f732a28673c4da61e3308027
Hey, Teacher <heyteacher70@gmail.com>
2023-08-16 11:25:35 +0200

    skip file already backuped

--- src/app/backup-restore.service.ts
+++ src/app/backup-restore.service.ts
@@ -14,8 +14,11 @@ import { localize } from 'nativescript-localize/angular';
 })
 export class BackupRestoreService{
 
-  private endpoint: string
   private apiKey: string
+  private endpoint: string
+  private listEndpoint: string
+  private backupEndpoint: string
+  private restoreEndpoint: string
 
   constructor(
     private httpClient: HttpClient,
@@ -33,44 +36,83 @@ export class BackupRestoreService{
         AppSettingsDefaultValue.AWS_BACKUP_RESTORE_REGION
       );
       this.endpoint = `https://${endpointPrefix}.execute-api.${region}.amazonaws.com/Prod/`
+      this.listEndpoint = `${this.endpoint}list` 
+      this.backupEndpoint = `${this.endpoint}backup`
+      this.restoreEndpoint = `${this.endpoint}restore`
+  }
+
+  private async _backupTracks(remoteTracks, callback) {
+    const documents: fs.Folder = <fs.Folder>fs.knownFolders.documents();
+    const folder: fs.Folder = <fs.Folder>documents.getFolder(StoreService.TRACKS_FOLDER);
+    const entities: fs.FileSystemEntity[] = await folder.getEntities()
+    const observables: any[] = []
+    let skipped: number = 0 
+  
+    // load track locally stored
+    for (let index = 0; index < entities.length; index++) {
+      const entity = entities[index];
+
+      const key = entity.name.split('.')[0]
+      
+      if (remoteTracks[key]) {
+        trace.write(`BackupRestoreService._backupTracks: track ${entity.name} already backuped`, trace.categories.Debug)
+        skipped = skipped + 1
+        continue
+      }
+  
+      trace.write(`BackupRestoreService._backupTracks: track ${entity.name} to be backuped`, trace.categories.Debug)
+      if (entity.name.length != 20) continue;
+      const body = {
+        name: entity.name,
+        content: await folder.getFile(entity.name).readText()
+      }
+      observables.push(this.httpClient.post(
+        this.backupEndpoint, 
+        [body], 
+        { headers: new HttpHeaders({ 'X-API-Key': this.apiKey })
+      }))
+    }
+
+    // load today logs
+    const todayLogsBody = await this.storeService.getTodayLogsBody()
+    if (todayLogsBody != null) {
+      trace.write(`BackupRestoreService._backupTracks: found today logs ${todayLogsBody.name}`, trace.categories.Debug)
+      observables.push(this.httpClient.post(
+        this.backupEndpoint, 
+        [todayLogsBody], 
+        { headers: new HttpHeaders({ 'X-API-Key': this.apiKey })
+      }))
+    }
+    else {
+      trace.write(`BackupRestoreService._backupTracks: today logs NOT found`, trace.categories.Debug)
+    }
+    if (observables.length == 0)   {
+      callback(localize(`no tracks backuped`))
+      return
+    }
+    forkJoin(observables).subscribe({
+      next: async data => {
+        callback(localize('%s tracks backuped, %s skipped',`${data.length}`,`${skipped}`))
+      },
+      complete: () => {
+        trace.write(`BackupRestoreService.backup: backups queued`, trace.categories.Debug)
+      },
+    })
   }
 
   async backup(callback): Promise<void> {
     try {
-
-      const documents: fs.Folder = <fs.Folder>fs.knownFolders.documents();
-      const folder: fs.Folder = <fs.Folder>documents.getFolder(StoreService.TRACKS_FOLDER);
-      const entities: fs.FileSystemEntity[] = await folder.getEntities()
-      const backupEndpoint: string = `${this.endpoint}backup` 
-
-      const observables: any[] = [] 
-      for (let index = 0; index < entities.length; index++) {
-        const entity = entities[index];
-        trace.write(`BackupRestoreService.backup: track ${entity.name}`, trace.categories.Error)
-        if (entity.name.length != 20) continue;
-        const body = {
-          name: entity.name,
-          content: await folder.getFile(entity.name).readText()
-        }
-        observables.push(this.httpClient.post(backupEndpoint, [body], { headers: new HttpHeaders({ 'X-API-Key': this.apiKey }) }))
-      }
-
-      if (observables.length == 0)   {
-        callback(localize(`no tracks backuped`))
-        return
-      }
-
-      forkJoin(observables).subscribe({
-        next: async data => {
-          callback(localize('%s tracks backuped',`${data.length}`))
-        },
-        complete: () => {
-          trace.write(`BackupRestoreService.backup: backups queued`, trace.categories.Debug)
-        },
+      const remoteTracks = {}
+      this.httpClient.get(this.listEndpoint, { headers: new HttpHeaders({ 'X-API-Key': this.apiKey }) }).subscribe((data: AWSListResponse) => {
+        trace.write(`BackupRestoreService.backup: list response ${data.tracks.length} tracks`, trace.categories.Debug)
+        for (let index = 0; index < data.tracks.length; index++) {
+          const key = data.tracks[index].key.split('/')[1].split('.')[0];
+          remoteTracks[key] = key
+        } 
+        this._backupTracks(remoteTracks, callback)
       })
-
     } catch (err) {
-      trace.write(`BackupRestoreService.backup: There was an error o ${err.nessage}`, trace.categories.Error)   
+      trace.write(`BackupRestoreService.backup: error on list remote tracks ${err.nessage}`, trace.categories.Error)   
       callback(localize('error on tracks backup: %s',err.message))
       throw err
      }
@@ -78,24 +120,28 @@ export class BackupRestoreService{
   
   async restore(callback): Promise<void> {
     try {
-      const listEndpoint: string = `${this.endpoint}list` 
-      const restoreEndpoint: string = `${this.endpoint}restore` 
- 
-      this.httpClient.get(listEndpoint, { headers: new HttpHeaders({ 'X-API-Key': this.apiKey }) }).subscribe((data: AWSListResponse) => {
+      this.httpClient.get(this.listEndpoint, { headers: new HttpHeaders({ 'X-API-Key': this.apiKey }) }).subscribe((data: AWSListResponse) => {
         trace.write(`BackupRestoreService.restore: list response ${data.tracks.length} tracks`, trace.categories.Debug)
  
         const observables: any[] = [] 
         for (let index = 0; index < data.tracks.length; index++) {
-          const key = data.tracks[index].key;
-          trace.write(`BackupRestoreService.restore: track ${key}`, trace.categories.Debug)
-          observables.push(this.httpClient.get(`${restoreEndpoint}?key=${key}`, { headers: new HttpHeaders({ 'X-API-Key': this.apiKey }) }))        
+          let key:string = data.tracks[index].key;
+          //key = key.replace('tracks/','').replace('.json','')
+          //if (this.storeService.getTrack(key) == null) {
+            trace.write(`BackupRestoreService.restore: track ${key} to restore`, trace.categories.Debug)
+            observables.push(
+              this.httpClient.get(`${this.restoreEndpoint}?key=${key}`, 
+              { headers: new HttpHeaders({ 'X-API-Key': this.apiKey }) }
+            ))
+          //} 
+          //else {
+          //  trace.write(`BackupRestoreService.restore: track ${key} already restored`, trace.categories.Debug)
+          //}
         }
-        
         if (observables.length == 0)   {
           callback(localize(`no tracks restored`))
           return
         }
-  
         forkJoin(observables).subscribe({
             next: async data => {
               for (let index = 0; index < data.length; index++) {
